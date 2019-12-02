29bf499bece0ed21f8a258aca494bfe0abef16cc
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2019-12-02 23:45:35 +0200

    This was an initial commit, from bash

---
+++ cmake-build-debug/CMakeFiles/Mmn11.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mmn11.dir/translate.c.o"
+  "Mmn11.exe"
+  "Mmn11.pdb"
+  "libMmn11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mmn11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ translate.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BINARY_FACTOR 2
+#define DECIMAL_FACTOR 10
+
+//TODO: Adapt the code to working with arrays.
+
+
+/*!
+ *The function gets a decimal number as a string and convert ot to int in order to de some
+ * calculations on it.
+ * @param dec_chr is the number as a string.
+ * @return the original number as binary number.
+ */
+long translate_dec_hex(char dec_chr[]){
+    long long binaryNum = 0;/* initialization*/
+    long long temp =1;
+    int rem = 1;/* initialization*/
+    long long decNum = atoll(dec_chr);/* convert char to int.*/
+    while (decNum != 0) {
+        rem = decNum%BINARY_FACTOR;
+        decNum = decNum / BINARY_FACTOR;
+        binaryNum +=  rem*temp;
+        temp *=  DECIMAL_FACTOR;/* setting the length of the binary number*/
+    }
+    return binaryNum;
+
+
+}
+
+
+/*!
+ * Runs the program; Getting the input number from the user and prints the equivalent binary form.
+ * @return
+ */
+int main() {
+    char decChr[100];/* The user can type an integer of up to 100 digits.*/
+    printf("Please enter a decimal number: ");
+    scanf("%s", decChr);/* Getting the input from user.*/
+    long long binaryNumber = translate_dec_hex(decChr);
+    printf("%llu", binaryNumber);
+    return 0;
+}
